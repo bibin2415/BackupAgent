0efcf9cc7645e381f4210c718cf8c9bd12509b05
Bibin Baby <bibin.baby@hrblock.com>
2021-05-17 13:41:36 +0530

    initial commint

--- app/src/main/java/com/hrb/backupagent/MainActivity.kt
+++ app/src/main/java/com/hrb/backupagent/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.hrb.backupagent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sharedpreferences =
+            getSharedPreferences("sample", Context.MODE_PRIVATE)
+        val data = sharedpreferences.getString("key", null)
+        if (data == null) {
+            writeData(sharedpreferences)
+        } else {
+            Log.d("From preference", data.toString())
+        }
+
+    }
+
+    private fun writeData(sharedpreferences: SharedPreferences) {
+        val editor: SharedPreferences.Editor = sharedpreferences.edit()
+        editor.putString("key", "value")
+        editor.apply()
     }
 }
